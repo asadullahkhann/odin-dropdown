741fccd95ef7eeb9f14757aca66048ab11db5bde
Asad <asadullahkhan0098@gmail.com>
2024-11-20 01:53:08 +0530

    Toggle button's icon when clicked

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import './styles.css';
 import { toggleDropdown } from './toggle-dropdown';
 
 const toggleDropdownBtn = document.querySelector('.toggle-dropdown>button');
+const buttonImgs = document.querySelectorAll('button>img');
 const dropdown = document.querySelector('.dropdown');
 
 toggleDropdownBtn.addEventListener('click', () => {
     toggleDropdown(dropdown);
+    buttonImgs.forEach(buttonImg => {
+        buttonImg.classList.toggle('hide');
+    })
 });
--- src/toggle-dropdown.js
+++ src/toggle-dropdown.js
@@ -1,6 +1,5 @@
 function toggleDropdown(dropdown) {
     dropdown.classList.toggle('hide');
-    console.log('works')
 }
 
 export { toggleDropdown };
